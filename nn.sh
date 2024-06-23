f3ca25ba6aa02f67f1d4e1dafec78db884d10699
Lucas Arruda <lucas123manoel4@gmail.com>
2024-06-23 13:11:36 -0300

    added new notes script

--- nn.sh
+++ nn.sh
@@ -1,6 +1,26 @@
-#!bin/bash
+#!/bin/bash
 
-today=$(date +"%Y-%m-%d")
+# Get today's date in the format YYYY-MM-DD
+TODAY=$(date +%Y-%m-%d)
 
-echo $today >> fmc2.adoc
+# Define the filename and directory
+DIRECTORY="./fmc2"
+FILENAME="${DIRECTORY}/${TODAY}.adoc"
+INCLUDE_TEXT="include::fmc2/${TODAY}.adoc[]"
 
+# Create the directory if it doesn't exist
+mkdir -p "$DIRECTORY"
+
+# Create the .adoc file with the specified content
+echo "== ${TODAY}
+
+:hardbreaks-option:
+
+===" > "$FILENAME"
+
+# Add the include text to ./fmc.adoc
+echo "
+$INCLUDE_TEXT" >> ./fmc2.adoc
+
+# Confirm completion
+echo "File ${FILENAME} created and included in ./fmc.adoc"
