c924d501a49739c807f653e9ed95ef2e46811144
lucasarruda12 <lucas123manoel4@gmail.com>
2024-11-26 18:03:24 -0300

    edb2 aula8

---
+++ jd-parser.sh
@@ -0,0 +1,22 @@
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 filename"
+    exit 1
+fi
+
+original_file=$1
+
+if  [[ $(basename "$original_file") =~ ^.{2}\..{2}\.md$ ]]; then
+
+    title=$(head -n 1 "$original_file" | sed 's/^# //')
+
+    new_file="${original_file%.*} $title.md"
+
+    mv  "$original_file" "$new_file"
+
+    sed -i 's/^## /# /' "$new_file"
+
+    sed -i '1,2d' "$new_file"
+
+else
+    echo "invalid filename"
+fi
