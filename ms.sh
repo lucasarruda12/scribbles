c66ce63457c44c5d7e6e6c2e4f488b3ade540c4a
lucasarruda12 <lucas123manoel4@gmail.com>
2025-01-22 18:08:25 -0300

    Wed Jan 22 18:08:25 -03 2025

--- ms.sh
+++
@@ -1,6 +0,0 @@
-# ms - Make Summary
-#
-# Build a sumarry file from a directory
-
-cd $1 && tree . -fi --filesfirst | sort -d | awk -F/ '{if ($NF ~ /\.md$/) { print "- [" $NF "](" $0 ")"} else {print "\n# " $NF "\n"}}' | sed '/u[0-9]/ s/^/\t/'
-
